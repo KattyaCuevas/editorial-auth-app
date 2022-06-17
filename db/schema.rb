e29ecf6c705e6f0ba0b3829aaa9d9e5c2de2d3ff
Kattya Cuevas <kattya.cuevas@gmail.com>
2022-06-17 13:27:54 -0500

    Add BCrypt and set up basic config

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_16_225630) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_17_182006) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "user_id", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_16_225630) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "posts"
