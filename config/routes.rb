c24a4d77f37098728afc1e0c6d9bb827b681efed
Kattya Cuevas <kattya.cuevas@gmail.com>
2022-06-17 15:21:21 -0500

    Add Sign Up feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :edit, :update]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
